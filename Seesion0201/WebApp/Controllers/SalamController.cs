004b5b0da923a614c62a97e69d4d045435e2f2e5
Mohammad Mirmostafa <mirmostafa@hotmail.com>
2024-10-21 22:01:53 +0330

    Initial commit

---
+++ Seesion0201/WebApp/Controllers/SalamController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+[ApiController]
+[Route("Bye")]
+public class SalamController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult HowAreYou()
+    {
+        return this.Ok();
+    }
+}
